9baab1bba371a54a605741cc02ff50688dbc17bf
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2024-09-20 22:43:05 -0300

    alteração no JS

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
 
-button.addEventListener('click', function () {
+button.addEventListener('click', () => {
   mobileNavbar.classList.toggle('active');
 });
 
